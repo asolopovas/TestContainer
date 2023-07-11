595f0858a70b411118d6d714cfdd7e78eda5e263
andrius <andrius.solopovas@gmail.com>
2023-07-11 22:00:35 +0100

    save

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-UNATTENDED=${UNATTENDED:-false}
-FORCE=${FORCE:-false}
+OS=$(awk -F= '/^ID=/ {gsub(/"/, "", $2); print tolower($2)}' /etc/os-release)
 NODE_VERSION=${NODE_VERSION:-18.16.1}
-VOLTA_DIR="$HOME/.volta"
+FORCE=${FORCE:-false}
+NVM_VERSION=${NVM_VERSION:-v0.39.3}
 
-if [ "$OS" = "alpine" ]; then
-    echo "export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release" >> .profile
-    nvm_get_arch() { nvm_echo \"x64-musl\"; }
+if [ "$FORCE" = "true" ]; then
+    echo "Force install nodejs"
+    rm -rf ~/.nvm
 fi
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+if [ "$OS" = "alpine" ]; then
+    export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release
+    NVM_DIR="$HOME/.nvm"
+    echo 'nvm_get_arch() { nvm_echo "x64-musl"; }' >> ~/.profile
+    source .profile
+fi
+
+PROFILE=/dev/null curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
+# source "$HOME/.nvm/nvm.sh"
+# nvm install $NODE_VERSION
